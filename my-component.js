c707bbc5b5180656bdadf17802216ec6349c10c2
taniaclemons <122949446+taniaclemons@users.noreply.github.com>
2025-03-14 17:15:04 -0700

    Update my-component.js

--- my-component.js
+++ my-component.js
@@ -7,4 +7,20 @@ connectedCallback() {
   const style = document.createElement('style');
   style.textContent = getMyComponentCSS();
   const title = this.getAttribute("title") || "Unknown Title";
-  const img
+  const img = this.getAttribute("img") || "Unknown img";
+  const description = this.getAttribute("description") || "Unknown Description";
+  const link = this.getAttribute("link") || "Unknown Link";
+  this.innerHTML = `
+    <div class="proj">
+      <hgroup>
+      <h2 class="title">${title}</h2>
+      <p class="description">${description}</p>
+      <a class="link">${link}</a>
+      </hgroup>
+      <img src="${img}" alt="${title}">
+    </div>
+    `;
+  }
+}
+console.log("custom element defined");
+customElements.define('proj', MyProject);
